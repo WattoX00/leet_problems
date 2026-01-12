d040cc4bd2de812b84151e3d4b62fc2709259488
WattoX00 <korschzozinho@gmail.com>
2026-01-12 18:03:20 +0100

    Filing

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+problemUrl = 'https://leetcode-api-pied.vercel.app/problem/'
+userUrl = 'https://leetcode-api-pied.vercel.app/user/' # userName comes here
+
+@app.get('/user')
+def getUserJson():
+    res = requests.get(userUrl)
+    return res.json()
+
+@app.get('/problem/{id}')
+def getProblemById(id: str):
+    res = requests.get(problemUrl+id)
+    return res.json()
---
+++ backend/to_local.py
@@ -0,0 +1,81 @@
+from app import getUserJson, getProblemById
+import json
+from pathlib import Path
+
+
+# running !!!
+
+#makeUserData()
+
+#syncProblemData()
+
+
+
+# user_data.json
+
+def makeUserData():
+    userJson = getUserJson() #json
+
+    name = userJson['username']
+    avatarUrl = userJson['profile']['userAvatar']
+    realname = userJson['profile']['realName']
+    website = userJson['profile']['websites']
+    gitUrl = userJson['githubUrl']
+
+    acSubmit = userJson['submitStats']['acSubmissionNum']
+    totalSubmit = userJson['submitStats']['totalSubmissionNum']
+
+    userDataJson = {
+        "name": name,
+        "avatar": avatarUrl,
+        "realname": realname,
+        "website": website,
+        "giturl": gitUrl,
+        "accepted": acSubmit,
+        "total": totalSubmit
+        }
+    
+    writeUserDataJson(userDataJson)
+
+def writeUserDataJson(data):
+    with open('./data/user_data.json', 'w') as f:
+        return json.dump(data, f, indent=4)
+
+# problem_data.json
+
+def problm(id):
+
+    problemJson = getProblemById(id) #json
+
+    title = problemJson['title']
+    stats = problemJson['stats']
+    difficulty = problemJson['difficulty']
+    url = problemJson['url']
+
+    problemDataJson = {
+        "title": title,
+        "stats": stats,
+        "difficulty": difficulty,
+        "url": url
+    }
+
+    data = openProblemDataJson()
+    data['problems'][id] = problemDataJson
+    
+    writeProblemDataJson(data)
+
+def openProblemDataJson():
+    with open('./data/problem_data.json', 'r') as f:
+        return json.load(f)
+
+def writeProblemDataJson(data):
+    with open('./problem_data.json', 'w') as f:
+        return json.dump(data, f, indent=4)
+
+def syncProblemData():
+    data = openProblemDataJson()
+
+    for file in Path('./problems').iterdir():
+
+        if file.stem not in data['problems']:
+            problm(file.stem)
